d9d44d350127af510f9942804482716a80e84f7d
peter_alfie <alfie1104@gmail.com>
2023-07-31 08:17:44 +0900

    feat(projectform, formfield, custommenu): implementing a form for create-project

---
+++ components/CustomMenu.tsx
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+import { Menu } from "@headlessui/react";
+import Image from "next/image";
+
+type Props = {
+  title: string;
+  state: string;
+  filters: Array<string>;
+  setState: (value: string) => void;
+};
+
+const CustomMenu = ({ title, state, filters, setState }: Props) => {
+  return <div>CustomMenu</div>;
+};
+
+export default CustomMenu;
