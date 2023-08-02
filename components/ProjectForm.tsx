e08676b73a86a20758f78b3389b31f0ffc1cb2de
peter_alfie <alfie1104@gmail.com>
2023-08-03 08:52:16 +0900

    feat(edit-project/[id]): implement edit project page

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,21 +1,22 @@
 "use client";
 
-import { SessionInterface } from "@/common.types";
+import { ProjectInterface, SessionInterface } from "@/common.types";
 import Image from "next/image";
 import FormField from "./FormField";
 import { categoryFilters } from "@/constants";
 import CustomMenu from "./CustomMenu";
 import { useState } from "react";
 import Button from "./Button";
-import { createNewProject, fetchToken } from "@/lib/actions";
+import { createNewProject, fetchToken, updateProject } from "@/lib/actions";
 import { useRouter } from "next/navigation";
 
 type Props = {
   type: string;
   session: SessionInterface;
+  project?: ProjectInterface;
 };
 
-const ProjectForm = ({ type, session }: Props) => {
+const ProjectForm = ({ type, session, project }: Props) => {
   const router = useRouter();
   const handleFormSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -28,6 +29,10 @@ const ProjectForm = ({ type, session }: Props) => {
       if (type === "create") {
         await createNewProject(form, session?.user?.id, token);
         // create project
+        router.push("/");
+      } else if (type === "edit") {
+        await updateProject(form, project?.id as string, token);
+
         router.push("/");
       }
     } catch (error) {
@@ -67,12 +72,12 @@ const ProjectForm = ({ type, session }: Props) => {
 
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [form, setForm] = useState({
-    title: "",
-    description: "",
-    image: "",
-    liveSiteUrl: "",
-    githubUrl: "",
-    category: "",
+    title: project?.title || "",
+    description: project?.description || "",
+    image: project?.image || "",
+    liveSiteUrl: project?.liveSiteUrl || "",
+    githubUrl: project?.githubUrl || "",
+    category: project?.category || "",
   });
 
   return (
